7051186c7568678bf2c2b9dfd74ecdc5b425f71c
Jimmy200 <bruno_pier@hotmail.com>
2021-05-26 12:47:35 -0300

    Adds the removeZero function for calculator_1

--- calculator_1/script.js
+++ calculator_1/script.js
@@ -1,3 +1,11 @@
 function clearDisplay() {
   document.getElementById("outputDisplay").innerHTML = "0";
 }
+
+function removeZero() {
+  let value = document.getElementById("outputDisplay").innerHTML;
+  if (value === "0") {
+    value = " ";
+    document.getElementById("outputDisplay").innerHTML = value;
+  }
+}
