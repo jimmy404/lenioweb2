8c1ae3953dbd06c0d9b707073d01e2e3a6e3605f
Jimmy200 <bruno_pier@hotmail.com>
2021-05-26 13:23:42 -0300

    Adds global variables for calculator_2

--- calculator_2/script.js
+++ calculator_2/script.js
@@ -1,4 +1,9 @@
-const numberButton   = document.getElementsByName('number');
-const operatorButton = document.getElementsByName('operator');
-const equalButton    = document.getElementsByName('equal')[0];
-const deleteButton   = document.getElementsByName('delete')[0];
+const numberButton = document.getElementsByName('number'),
+  operatorButton   = document.getElementsByName('operator'),
+  equalButton      = document.getElementsByName('equal')[0],
+  deleteButton     = document.getElementsByName('delete')[0];
+
+let result          = document.getElementById('result'),
+  currentOperation  = '',
+  previousOperation = '',
+  operation         = undefined;
