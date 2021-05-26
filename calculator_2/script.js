5f8002ece0fa232f9ef0556041a44ecb03517a1d
Jimmy200 <bruno_pier@hotmail.com>
2021-05-26 13:19:08 -0300

    Adds variables for captured DOM elements

---
+++ calculator_2/script.js
@@ -0,0 +1,4 @@
+const numberButton   = document.getElementsByName('number');
+const operatorButton = document.getElementsByName('operator');
+const equalButton    = document.getElementsByName('equal')[0];
+const deleteButton   = document.getElementsByName('delete')[0];
